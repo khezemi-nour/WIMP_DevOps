c3ae93d4ddd84f9d8f02e5bd9009b743fcc91e8d
yahia <yahiafellah@yahoo.fr>
2023-02-15 10:35:47 -0500

    [create] define gateway route file

---
+++ gateway/routes/routes.js
@@ -0,0 +1,22 @@
+
+const API_PREFIX = 'api/v1'
+
+exports.routes = [
+
+    /*
+     * THIS ROUTE HAS BEEN GIVEN AS EXAMPLE ROUTE 
+    {
+        url:`/${API_PREFIX}/auth`,
+        proxy: {
+            target:"http://[::1]:3001/auth",
+            changeOrigin : true,
+            pathRewrite: {
+                [`^/${API_PREFIX}/auth`]: '',
+            },
+        }
+    },
+    */
+
+
+]
+
