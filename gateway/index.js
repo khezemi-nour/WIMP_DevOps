24160740017ba862b834b62b5075b2e5108416be
yahia <yahiafellah@yahoo.fr>
2023-02-15 10:30:38 -0500

    [create] create main gateway file

---
+++ gateway/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+///enabling CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"
+  );
+  res.header("Access-Control-Expose-Headers", "Content-Length");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Accept, Authorization, Content-Type, X-Requested-With, Range,X-Auth"
+  );
+  if (req.method == "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+// listening
+app.listen(PORT ,()=> {
+    console.log("Gateway running on port :" + PORT);
+})
