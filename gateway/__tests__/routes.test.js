19b56ec6c3859eee2405df5e4db7217b353b2553
yahia <yahiafellah@yahoo.fr>
2023-02-25 17:11:06 -0500

    [misc] change the naming of the test file

--- gateway/__tests__/routes.test.js
+++ gateway/__tests__/routes.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require(".././app");
 
-describe("Test the root path", () => {
+describe("Test the root path to make sure its running fine", () => {
   test("It should response the GET method", done => {
     request(app)
       .get("/")
