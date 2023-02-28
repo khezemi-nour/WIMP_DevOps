c5778923c35ccac770323025b0ede0712be0299a
yahia <yahiafellah@yahoo.fr>
2023-02-28 11:45:53 -0500

    [update] route test js  - make the right testing for create user

--- users/__test__/route.test.js
+++ users/__test__/route.test.js
@@ -2,25 +2,27 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("Test the root path to make sure its running fine", () => {
-  test("It should response the GET method", done => {
+  test("It should response the GET method", (done) => {
     request(app)
       .get("/")
-      .then(response => {
+      .then((response) => {
         expect(response.statusCode).toBe(200);
         done();
       });
   });
 });
 
-describe('POST /users', () => {
-  test('creates a new user', async () => {
-    const user = { name: 'Alice', email: 'alice@example.com' };
-    const response = await request(app)
-      .post('/users')
-      .send(user)
-      .expect(201);
+describe("POST /users", () => {
+  test("creates a new user", async () => {
+    const user = {
+      firstName: "admin",
+      lastName: "admin",
+      userName: "admin",
+      password: "admin",
+      permissionLevel: 1,
+    };
+    const response = await request(app).post("/users").send(user).expect(201);
 
-    expect(response.body).toMatchObject(user);
-    expect(users).toContainEqual(user);
+    expect(response.body).toHaveProperty('id')
   });
 });
