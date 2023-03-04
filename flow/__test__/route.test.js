58ded21323600e44575bd1eb64ebc08104558eb0
yahia <yahiafellah@yahoo.fr>
2023-03-03 20:06:06 -0500

    [create] tests for flow service

---
+++ flow/__test__/route.test.js
@@ -0,0 +1,24 @@
+const request = require("supertest");
+const app = require("../app");
+const { connect, clearAllCollections, close } = require('./db.test');
+
+beforeEach(async () => {
+  await connect();
+  await clearAllCollections();
+});
+
+afterAll(async () => {
+  await clearAllCollections();
+  await close();
+});
+
+describe("Test the root path to make sure its running fine", () => {
+  test("It should response the GET method", (done) => {
+    request(app)
+      .get("/")
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
