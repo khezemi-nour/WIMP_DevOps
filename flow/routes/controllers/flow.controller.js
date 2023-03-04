2be48015ad465cd20e4861d99d70379c0db0bf9b
yahia <yahiafellah@yahoo.fr>
2023-03-03 19:52:31 -0500

    [update] create routes for flow service

---
+++ flow/routes/controllers/flow.controller.js
@@ -0,0 +1,75 @@
+const axios = require("axios");
+const flowModel = require("../models/flows.model");
+
+
+/*
+ Use this when u call the /flows in the headers of the requests
+*/
+// const config = {
+//   headers: {
+//     "Node-RED-API-Version": "v2",
+//     "Node-RED-Deployment-Type": "full",
+//   },
+// };
+
+create_flow_data = (req) => {
+  // check if _data is an array
+  return {
+    id: req._id,
+    label: req.jwt.name.split(" ")[0],
+    nodes: Array.isArray(req.body.flowData) ? req.body.flowData : Array(req.body.flowData),
+  };
+};
+exports.create = async (res, req) => {
+  try {
+    const result = await axios.post(
+      process.env.FLOW_URL,
+      create_flow_data(req)
+    );
+    await flowModel.pathFlowById( req._id, { flowId : result.data.id});
+    res.status(201).send({ id: req.id, flowId: result.data.id });
+  } catch (error) {
+    res.status(409).send({
+      errors:
+        error && error.response && error.response.data
+          ? error.response.data
+          : error,
+    });
+  }
+};
+
+//TODO: Send update flow for the concerned user by using the ID
+exports.insert = async (req,res) => {
+    try{
+        const result = await axios.post(
+            process.env.FLOW_URL,
+            create_flow_data(req)
+          );
+        await flowModel.pathFlowById( req._id, { flowId : result.data.id});
+        res.status(201).send({ 
+          flowId:result.data.id,
+          message : "flow loaded successfully"
+        })
+    }catch(error){
+      console.log(error)
+        res.status(409).send({
+            errors:
+              error && error.response && error.response.data
+                ? error.response.data
+                : error,
+          })
+    }
+
+}
+
+/**
+ * Get all the flows is running now 
+ * @returns {Array} list of flows
+ */
+
+exports.list = () => {
+  return axios.get(process.env.FLOWS_URL);
+}
+
+
+
---
+++ flow/routes/models/flow.model.js
@@ -0,0 +1,133 @@
+const mongoose = require("mongoose")
+require('dotenv').config()
+mongoose.connect(process.env.DB_URL,{ useUnifiedTopology: true });
+const Schema = mongoose.Schema;
+
+const flowSchema = new Schema({
+    userId: String,
+    flowId : String,
+    name : String,
+    data :Object
+}
+)
+
+flowSchema.virtual('id').get(function() {
+    return this._id.toHexString();
+})
+
+flowSchema.set('toJSON',{
+    virtuals:true
+})
+
+flowSchema.findById = function(cb){
+    return this.model('flows').find({flowId:this.id},cb)
+}
+
+
+const Flow = mongoose.model('flows',flowSchema);
+
+checkIfExists = async (data) => {
+    return await Flow.exists(data);
+}
+
+
+
+exports.findById = (id) => {
+    return Flow.findById(id).then((result) => {
+        result = result.toJSON();
+        delete result.__id;
+        delete result.__v;
+        return result;
+    })
+}
+
+exports.create = (data) => {
+    const flow = new Flow(data);
+    try{
+        return  flow.save(); 
+    }catch(err){
+        return new Promise((resolve,reject) => resolve(null));
+    }
+
+}
+
+exports.list = (perPage, page) => {
+    return new Promise((resolve, reject) => {
+        Device.find()
+            .limit(perPage)
+            .skip(perPage * page)
+            .exec(function (err, data) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+            })
+    });
+};
+
+exports.put = (id,data) => {
+    return new Promise((resolve, reject) => {
+        Device.findByIdAndUpdate(id,data,function (err,device) {
+            if (err) reject(err);
+            resolve(device);
+        });
+    });
+};
+
+exports.pathFlowById = (id,data) => {
+    return Flow.findByIdAndUpdate(id,data);
+}
+exports.patchFlowByFlowId = (id,data) => {
+    return Flow.findOneAndUpdate({
+        flowId:id,
+    },data,{
+        returnOriginal: true
+      });    
+}
+
+
+exports.patchFlowId = (id,flowId) => {
+      
+    return new Promise((resolve,reject) => {
+        Flow.findById(id,function(err,flow){
+            if(err) reject(err);
+            flow.flowId = flowId
+
+            flow.save(function(err,updates){
+                if(err) reject(err);
+                resolve(updates);
+            })
+        });
+    })
+}
+
+exports.patch = (id, data) => {
+    return new Promise((resolve, reject) => {
+        Flow.findById(id, function (err, flow) {
+            if (err) reject(err);
+            let actualPermisssion = flow.permissionLevel;
+            for (let i in data) {
+                flow[i] = data[i];
+            }
+            flow.permissionLevel = actualPermisssion;
+            flow.save(function (err, updates) {
+                if (err) return reject(err);
+                resolve(updates);
+            });
+        });
+    });
+
+};
+
+exports.removeById = (id) => {
+    return new Promise((resolve, reject) => {
+        Device.remove({_id: id}, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(err);
+            }
+        });
+    });
+};
