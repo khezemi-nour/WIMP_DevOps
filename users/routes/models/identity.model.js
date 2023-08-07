6473203a6ce5c8dacd91dece6f04c5175c69fd7d
YahiaFellah <33671463+yahiaelfellah@users.noreply.github.com>
2023-08-07 11:36:35 -0400

    [update] user microservice

--- users/routes/models/identity.model.js
+++ users/routes/models/identity.model.js
@@ -1,102 +1,107 @@
-const mongoose = require('mongoose');
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env' )});
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 
-mongoose.connect(process.env.mongoDbUrl || "mongodb://localhost:27017/myapp",{ useUnifiedTopology: true });
+mongoose.connect(process.env.mongoDbUrl || "mongodb://localhost:27017/WIMPv2", {
+  useUnifiedTopology: true,
+  bufferCommands:true,
+  useNewUrlParser: true,
+  autoCreate:true,
+});
+mongoose.set('debug', true);
+
 const Schema = mongoose.Schema;
 
-const identiySchema = new Schema({
+const identiySchema = new Schema(
+  {
     firstName: String,
     lastName: String,
     email: String,
     birthday: Date,
-    userName:String,
+    userName: String,
     password: String,
     permissionLevel: Number,
-    departement : String,
-    status: Array, 
-    devices:Array,
-    noderedInstance: Object
-},{ timestamps: true });
+    departement: String,
+    status: Array,
+    devices: Array,
+    noderedInstance: Object,
+  },
+  { timestamps: true }
+);
 
-identiySchema.virtual('id').get(function () {
-    return this._id.toHexString();
+identiySchema.virtual("id").get(function () {
+  return this._id.toHexString();
 });
 
 // Ensure virtual fields are serialised.
-identiySchema.set('toJSON', {
-    virtuals: true
+identiySchema.set("toJSON", {
+  virtuals: true,
 });
 
 identiySchema.findById = function (cb) {
-    return this.model('Users').find({id: this.id}, cb);
+  return this.model("Users").find({ id: this.id }, cb);
 };
 
-const Identity = mongoose.model('Users', identiySchema);
+const Identity = mongoose.model("Users", identiySchema);
 
 exports.findByEmail = (email) => {
-    return Identity.find({email: email});
+  return Identity.find({ email: email });
 };
 exports.findById = (id) => {
-    return Identity.findById(id)
-        .then((result) => {
-            result = result.toJSON();
-            delete result._id;
-            delete result.__v;
-            return result;
-        });
+  return Identity.findById(id).then((result) => {
+    result = result.toJSON();
+    delete result._id;
+    delete result.__v;
+    return result;
+  });
 };
 
-exports.findByUserName = (name) =>  {
-    return Identity.find({userName:name})
-}
+exports.findByUserName = (name) => {
+  return Identity.find({ userName: name });
+};
 
 exports.createIdentity = (userData) => {
-    console.log(userData);
-    const user = new Identity(userData);
-    return user.save();
+  console.log(userData);
+  const user = new Identity(userData);
+  return user.save();
 };
 
 exports.list = (perPage, page) => {
-    return new Promise((resolve, reject) => {
-        Identity.find()
-            .limit(perPage)
-            .skip(perPage * page)
+  return new Promise((resolve, reject) => {
+    Identity.find()
+      .limit(perPage)
+      .skip(perPage * page)
 
-            .exec().then((users)  => {
-                resolve(users);
-                
-            }).catch((err) => { 
-                reject(err);
-
-             }) 
-    });
-};
-
-exports.putIdentity = (id,data) => {
-    return new Promise((resolve, reject) => {
-        Identity.findByIdAndUpdate(id,data).then(
-           function(user) {
-               return resolve(user);
-        
-        }).catch((err) => { 
+      .exec()
+      .then((users) => {
+        resolve(users);
+      })
+      .catch((err) => {
         reject(err);
-
-     }) 
-    });
+      });
+  });
 };
 
+exports.putIdentity = (id, data) => {
+  return new Promise((resolve, reject) => {
+    Identity.findByIdAndUpdate(id, data)
+      .then(function (user) {
+        return resolve(user);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
 
 exports.removeById = (id) => {
-    return new Promise((resolve, reject) => {
-        Identity.deleteOne({_id: id}).then(
-            function(user) {
-                return resolve(user);
-         
-         }).catch((err) => { 
-         reject(err);
- 
-      })      
-    });
+  return new Promise((resolve, reject) => {
+    Identity.deleteOne({ _id: id })
+      .then(function (user) {
+        return resolve(user);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 };
-
--- users/webpack.config.js
+++ users/webpack.config.js
@@ -1,21 +1,23 @@
-const path = require('path')
-const webpack = require('webpack')
-const nodeExternals = require('webpack-node-externals')
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const nodeExternals = require("webpack-node-externals");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const WebpackBar = require('webpackbar');
+
 module.exports = {
   entry: {
-    server: './index.js',
+    server: "./index.js",
   },
   output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].js'
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].js",
   },
-  target: 'node',
+  target:  ["node"],
   node: {
     // Need this when working with express, otherwise the build fails
-    __dirname: false,   // if you don't put this is, __dirname
-    __filename: false,  // and __filename return blank or /
+    __dirname: false, // if you don't put this is, __dirname
+    __filename: false, // and __filename return blank or /
   },
   externals: [nodeExternals()], // Need this to avoid error when working with Express
   module: {
@@ -25,22 +27,28 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
-    //   {
-    //     // Loads the javacript into html template provided.
-    //     // Entry point is set below in HtmlWebPackPlugin in Plugins 
-    //     test: /\.html$/,
-    //     use: [{loader: "html-loader"}]
-    //   }
-    ]
+      { test: /\.node$/, use: "node-loader" },
+      //   {
+      //     // Loads the javacript into html template provided.
+      //     // Entry point is set below in HtmlWebPackPlugin in Plugins
+      //     test: /\.html$/,
+      //     use: [{loader: "html-loader"}]
+      //   }
+    ],
   },
   plugins: [
     new CopyWebpackPlugin({
-      patterns: [
-        { from: 'security/tls', to: 'security/tls' },
-      ]
-    })
-  ]
-}
+      patterns: [{ from: "security/tls", to: "security/tls" }],
+    }),
+    // // Add a plugin to provide more information during build
+    // new webpack.ProgressPlugin((percentage, message) => {
+    //   // This will log the build progress and message to the console
+    //   console.log(`${(percentage * 100).toFixed()}% ${message}`);
+    // }),
+    new WebpackBar(),
+
+  ],
+};
