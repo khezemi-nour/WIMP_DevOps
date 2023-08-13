4f565a83fa9552c7b3abf3008f9ba7aecc38a84c
YahiaFellah <33671463+yahiaelfellah@users.noreply.github.com>
2023-08-13 12:32:35 -0400

    [update] security script

--- users/security/tls/security_init.bat
+++ users/security/tls/security_init.bat
@@ -1,6 +1,20 @@
+@REM @echo off
+
+@REM :: Generate private key
+@REM ssh-keygen -t rsa -b 4096 -m PEM -f private.key -E SHA512
+
+@REM echo Private and public keys generated successfully.
+
 @echo off
+
+:: Specify the target folder where you want to copy the keys
+set "target_folder=../../../gateway/security/tls"
 
 :: Generate private key
 ssh-keygen -t rsa -b 4096 -m PEM -f private.key -E SHA512
 
-echo Private and public keys generated successfully.
+:: Copy the keys to the target folder
+copy private.key "%target_folder%"
+copy private.key.pub "%target_folder%"
+
+echo Private and public keys generated and copied to "%target_folder%" successfully.
--- users/security/tls/security_init.sh
+++ users/security/tls/security_init.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+
+# Specify the relative target folder where you want to copy the keys
+target_folder="../../../gateway/security/tls"
+
+
 # Generate public key from private key
 ssh-keygen -t rsa -b 4096 -m PEM -f private.key -E SHA512
 
 echo "Private and public keys generated successfully."
+
+# Copy the keys to the target folder
+cp private.key "$target_folder"
+cp private.key.pub "$target_folder"
+
+echo "Private and public keys generated and copied to $target_folder successfully."
